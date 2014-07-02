990e203abdc3e35e1acf65ee1540e8495116814b
c0ming <c0ming@qq.com>
2014-07-02 23:27:07 +0800

    Live Blur Test

--- LiveBlurTest/ViewController.m
+++ LiveBlurTest/ViewController.m
@@ -8,21 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-            
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate> {
+    UIVisualEffectView *_blurView;
+    
+    NSArray *_colorsArray;
+}
 
 @end
 
 @implementation ViewController
-            
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _colorsArray = @[[UIColor redColor], [UIColor blueColor], [UIColor greenColor], [UIColor yellowColor],
+                     [UIColor purpleColor], [UIColor cyanColor], [UIColor orangeColor]];
+    
+    // UIBlurEffect & UIVisualEffectView
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    _blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    _blurView.translatesAutoresizingMaskIntoConstraints = NO;
+    _blurView.frame = CGRectMake(60.0, 120.0, 200.0, 120.0);
+    _blurView.layer.borderColor = [UIColor whiteColor].CGColor;
+    _blurView.layer.borderWidth = 0.5;
+    _blurView.layer.cornerRadius = 2.0;
+    _blurView.layer.masksToBounds = YES;
+    [self.view addSubview:_blurView];
+
+    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_blurView);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[_blurView(200)]" options:0 metrics:nil views:viewsDictionary]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-120-[_blurView(120)]" options:0 metrics:nil views:viewsDictionary]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_blurView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewDidLayoutSubviews {
+    // add UILabel to UIVisualEffectView's contentView
+    UILabel *label = [[UILabel alloc] initWithFrame:_blurView.bounds];
+    label.textColor = [UIColor darkGrayColor];
+    label.font = [UIFont boldSystemFontOfSize:15.0];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"UIVisualEffectView";
+    [_blurView.contentView addSubview:label];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 30.0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *indentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:indentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:indentifier];
+    }
+    
+    cell.backgroundColor = _colorsArray[indexPath.row % _colorsArray.count];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", @(indexPath.row)];
+    
+    return cell;
 }
 
 @end
