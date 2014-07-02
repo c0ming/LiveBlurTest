7666403140e662c0c7ef6fe08b922d367099480a
c0ming <c0ming@c0ming.local>
2014-07-02 22:43:09 +0800

    Initial Commit

---
+++ LiveBlurTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveBlurTest
+//
+//  Created by c0ming on 14/7/2.
+//  Copyright (c) 2014年 c0ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveBlurTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveBlurTest
+//
+//  Created by c0ming on 14/7/2.
+//  Copyright (c) 2014年 c0ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
